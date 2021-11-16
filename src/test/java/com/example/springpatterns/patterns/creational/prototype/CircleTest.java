18ed5c398a32d294d34de54785c6b265283555eb
María de los Ángeles Barba <mangelesbarma@gmail.com>
2021-11-16 23:39:25 +0000

    Circle 100% Shape 100%

---
+++ src/test/java/com/example/springpatterns/patterns/creational/prototype/CircleTest.java
@@ -0,0 +1,26 @@
+package com.example.springpatterns.patterns.creational.prototype;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+
+class CircleTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void copy() {
+        int radius = 1;
+        Circle newCircle = new Circle("red",radius );
+
+        assertEquals(1,newCircle.radius);
+        assertEquals("red", newCircle.getColor());
+
+        Shape forma = newCircle.copy();
+    }
+}
